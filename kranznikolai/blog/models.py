e2d65687c2bf667751e87e4c636aa0a301daa90e
kranz912 <me@kranznikolai.com>
2020-02-21 22:52:22 -0500

    update

--- kranznikolai/blog/models.py
+++ kranznikolai/blog/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    STATUS = models.IntegerField(choices=STATUS, default=0)
+    status = models.IntegerField(choices=STATUS, default=0)
 
     class Meta:
         ordering = ['-created_on']
